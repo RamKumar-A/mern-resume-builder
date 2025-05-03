7a970781063a03f3dc7879fa233b22fcb24fb303
Ram Kumar <ramk41934@gmail.com>
2025-05-03 18:55:35 +0530

    Initial commit

--- frontend/resume-builder/src/utils/apiPaths.js
+++ frontend/resume-builder/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:8000';
+// export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'https://mern-resume-builder-backend.onrender.com';
 
 export const API_PATH = {
   AUTH: {
